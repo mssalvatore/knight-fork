d3b1425a8b799306e876e425d3fe8b48881616f7
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-10-04 22:18:43 -0400

    Avoid unnecessary page refreshing

--- js/knight-fork.js
+++ js/knight-fork.js
@@ -2,6 +2,7 @@ RelativeCoordinates = Chessboard.RelativeCoordinates
 
 const config = {
     draggable: false,
+    moveSpeed: 0,
     position: {},
     onContextmenuSquare: onContextmenuSquare,
     onMouseclickSquare: onMouseclickSquare
@@ -13,6 +14,7 @@ const target_pieces_no_king = ["R", "N", "B", "Q"];
 const target_color = ["w", "b"];
 
 var clicked_squares = [];
+var clicked_square_elements = [];
 var correct_squares = [];
 
 const board1 = ChessBoard('board1', config);
@@ -23,9 +25,14 @@ function toggleSelection(element, color) {
         let spread = Math.floor(blur/ 2);
         element.style.boxShadow = `0 0 ${blur}px ${spread}px `+ color + " inset";
     } else {
-        element.style.opacity = 1.0;
-        element.style.boxShadow = "";
+        deselect(element);
     }
+}
+
+function deselect(element) {
+    console.log("deselecting: " + element);
+    element.style.opacity = 1.0;
+    element.style.boxShadow = "";
 }
 
 function onMouseclickSquare (evt, square) {
@@ -34,10 +41,15 @@ function onMouseclickSquare (evt, square) {
 
     if (index >= 0) {
         clicked_squares.splice(index, 1);
+        clicked_square_elements.splice(index, 1);
     }
     else {
         clicked_squares.push(square_name);
+        clicked_square_elements.push(square);
     }
+
+    console.log(clicked_squares);
+    console.log(clicked_square_elements);
 
     toggleSelection(square, "#e6c912");
 }
@@ -47,11 +59,22 @@ function onContextmenuSquare (evt, square) {
 }
 
 function verify() {
-    if (clicked_squares.sort().join(",") == correct_squares.sort().join(",")) {
-        location.reload();
+    if (clicked_squares.toSorted().join(",") == correct_squares.sort().join(",")) {
+        clearSelectedSquares();
+        display_new_puzzle();
     } else {
         alert("Incorrect!");
     }
+}
+
+function clearSelectedSquares() {
+    clicked_square_elements.forEach((square) => {
+        deselect(square);
+    });
+
+    clicked_square_elements =[];
+    clicked_squares = [];
+
 }
 
 function square_add(square, relative) {
@@ -79,39 +102,42 @@ function get_enemy_coordinates() {
     return [file1, file_index_1, rank1, file2, rank2];
 }
 
+function display_new_puzzle() {
+    correct_squares = [];
+    const [file1, file_index_1, rank1, file2, rank2] = get_enemy_coordinates();
+    const square_1_name = get_square_name(file1, rank1);
+    const square_2_name = get_square_name(file2, rank2);
+    const square_1 = $('*[data-square="' + square_1_name + '"]')[0]
+    const square_2 = $('*[data-square="' + square_2_name + '"]')[0]
+
+    const color = target_color[Math.floor(Math.random() * target_color.length)]
+    const piece_1 = target_pieces[Math.floor(Math.random() * target_pieces.length)]
+    const piece_2 = target_pieces_no_king[Math.floor(Math.random() * target_pieces_no_king.length)]
+
+    var position = {
+        [square_1_name]:  color + piece_1,
+        [square_2_name]: color + piece_2
+    }
+
+    board1.position(position);
+
+    enemy_relationship = new RelativeCoordinates(file_index_2 - file_index_1, rank2 - rank1);
+
+    const fork_positions = Chessboard.KnightForkMap.get(enemy_relationship.toString()) || [];
+
+    fork_positions.forEach((potential_position) => {
+        potential_square = square_add(square_1_name, potential_position);
+        if (ChessBoard.validSquare(potential_square)) {
+            //toggleRed($('*[data-square="' + potential_square + '"]')[0]);
+            correct_squares.push(potential_square);
+        }
+    });
+}
+
 $(document).on("keypress", function(evt) {
     if (evt.which === 13 || evt.which === 99) {
         verify();
     }
 });
 
-const [file1, file_index_1, rank1, file2, rank2] = get_enemy_coordinates();
-const square_1_name = get_square_name(file1, rank1);
-const square_2_name = get_square_name(file2, rank2);
-const square_1 = $('*[data-square="' + square_1_name + '"]')[0]
-const square_2 = $('*[data-square="' + square_2_name + '"]')[0]
-
-const color = target_color[Math.floor(Math.random() * target_color.length)]
-const piece_1 = target_pieces[Math.floor(Math.random() * target_pieces.length)]
-const piece_2 = target_pieces_no_king[Math.floor(Math.random() * target_pieces_no_king.length)]
-
-var position = {
-    [square_1_name]:  color + piece_1,
-    [square_2_name]: color + piece_2
-}
-
-board1.position(position);
-
-enemy_relationship = new RelativeCoordinates(file_index_2 - file_index_1, rank2 - rank1);
-
-const fork_positions = Chessboard.KnightForkMap.get(enemy_relationship.toString()) || [];
-
-fork_positions.forEach((potential_position) => {
-    potential_square = square_add(square_1_name, potential_position);
-    if (ChessBoard.validSquare(potential_square)) {
-        //toggleRed($('*[data-square="' + potential_square + '"]')[0]);
-        correct_squares.push(potential_square);
-    }
-});
-
-
+$(document).ready(display_new_puzzle)
